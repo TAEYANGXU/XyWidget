0b73740feb0ec2d43ecd92b95a3ec18ce006afd1
徐琰璋 <xuyanzhang@lanyife.com>
2018-12-06 14:07:27 +0800

    AppConst update

--- XyWidget/Classes/Const/AppConst.h
+++ XyWidget/Classes/Const/AppConst.h
@@ -8,5 +8,17 @@
 #ifndef AppConst_h
 #define AppConst_h
 
+#define WS(weakSelf) __weak __typeof(&*self)weakSelf = self;
+
+#ifdef DEBUG // 开发
+#define Log(format, ...) NSLog((@"[LINE: %d]%s: " format), __LINE__, __PRETTY_FUNCTION__, ## __VA_ARGS__)
+#else // 发布
+#define Log(format, ...)
+#endif
+
+/**
+ *  app版本号
+ */
+#define APPVER  [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"]
 
 #endif /* AppConst_h */
